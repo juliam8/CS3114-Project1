1a1fc1e08ad3119e4c2d872d43cd94b01dfa6294
Julia Mahon Kuzin <juliam8@vt.edu>
2019-01-26 16:15:29 -0500

    First

---
+++ CS3114-Project1/src/Rectangle1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author jmkuz
+ *
+ */
+public class Rectangle1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+
+	}
+
+}
