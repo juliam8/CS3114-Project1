7361c27173e739eff7b414eaccaccc08cbeaa6f7
Julia Mahon Kuzin <juliam8@vt.edu>
2019-01-26 16:53:33 -0500

    Second commit

--- CS3114-Project1/src/Rectangle1.java
+++ CS3114-Project1/src/Rectangle1.java
@@ -6,14 +6,34 @@
  * @author jmkuz
  *
  */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;	
+
+
 public class Rectangle1 {
 
 	/**
-	 * @param args
+	 * describe the main here
+	 * @param args the arguments
+	 * @throws FileNotFoundException 
+	 *also do @return if needed
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Julia");
+		
+		Scanner scan = new Scanner(new File(args[0]));
+		String command = scan.next();
+		
+		while (scan.hasNext()) {
+			String temp = scan.next();
+			if (temp.equals("hi")) {
+				
+			}
+		}
+		scan.close();
 		
 
 	}
