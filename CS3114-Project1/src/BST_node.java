6061c14301c59fda263f6888a49e577d8a0c8b11
abbym1 <abbym1@vt.edu>
2019-01-27 16:01:45 -0500

    insert helper

--- CS3114-Project1/src/BST_node.java
+++ CS3114-Project1/src/BST_node.java
@@ -8,27 +8,19 @@
  */
 
 public class BST_node<T extends Comparable<T>> implements Comparable<BST_node<T>>{
-	
+	//constructors
+	BST_node(){ left = right = null; }
 	BST_node(T n){
 		key = n;
 		left = right = null;
-		
 	}
-	
-	/*BST_node(BST_node n){
-		key = n.get_key();
-		left = n.left;
-		right = n.right;
+	BST_node(T n, BST_node<T> l, BST_node<T> r){
+		key = n;
+		left = l;
+		right = r;
+	}
 
-		
-	}*/
-	
-	@Override
-    public int compareTo(BST_node<T> b) {
-        return key().compareTo(b.key());
-	}
-	
-	public void insert(T r) {
+	/*public void insert(T r) {
 		if (key.compareTo(r) <= 0) {
 			if (right == null) {
 				right = new BST_node<T>(r);
@@ -42,29 +34,32 @@ public class BST_node<T extends Comparable<T>> implements Comparable<BST_node<T>
 				left.insert(r);
 			}
 		}
-	}
-
-	public T key() {
-		return key;
-	}
+	}*/
 	
-	public void set_key(T k) {
-		key = k;
-	}
+	//Get and set the key value
+	public T key() { return key; }
+	public void set_key(T k) { key = k; }
 	
-	public BST_node<T> right() {
-		return right;
-	}
+	//get and set the left and right child nodes
+	public BST_node<T> right() { return right; }
+	public void set_r(BST_node<T> r) { right = r; }
 	
-	public BST_node<T> left() {
-		return left;
-	}
-
+	public BST_node<T> left() { return left; }
+	public void set_l(BST_node<T> l) { left = l; }
+	
+	//return true IFF this is a leaf node
+	public boolean isLeaf() { return (left == null && right == null); }
+	
 	public void print() {
 		System.out.print(this.key + "\n");
 	}
 	
+	@Override
+    public int compareTo(BST_node<T> b) {
+        return key().compareTo(b.key());
+	}
+	
+	//private data members
 	private BST_node<T> left, right;
 	private T key;
-	
 }
