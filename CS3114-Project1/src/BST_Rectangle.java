90062d3c479a114e26f8346be26867ff256c0dfd
Julia Mahon Kuzin <juliam8@vt.edu>
2019-02-04 22:06:24 -0500

    idk

--- CS3114-Project1/src/BST_Rectangle.java
+++ CS3114-Project1/src/BST_Rectangle.java
@@ -31,35 +31,6 @@ public class BST_Rectangle<K, D> extends BST<RectKey, RectData> {
 		return null;
 	}
 	
-	//remove the temp found by find_helper
-	/*protected BST_node<RectKey, RectData> remove_helper_data(BST_node<RectKey, RectData> rt, RectData data) {
-		//BST_node<RectKey, RectData> mytemp = null;
-		if (rt == null) return null;
-		if (rt.data().compareTo(data) == 0) {
-		  //  if (rt.left() != null)
-		    	//mytemp = rt.set_l(remove_helper_data(rt.left(), data));
-		  //  if (mytemp == null)
-		        //result = locate(p,famTree.right);
-	//			rt.set_l(remove_helper_data(rt.left(), data));
-		//	else if (rt.data().compareTo(data) < 0)
-			//	rt.set_r(remove_helper_data(rt.left(), data));
-		}
-		else {		
-			if (rt.left() == null)
-				return rt.right();
-			else if (rt.right() == null)
-				return rt.left();
-			else {	//two children
-				BST_node<RectKey, RectData> temp = get_min(rt.right());
-				rt.set_data(temp.data());
-				rt.set_key(temp.key());
-				rt.set_r(delete_min(rt.right()));
-			}
-		}
-		return rt;
-	}*/
-		
-	
 	BST_node<RectKey, RectData> find_helper_data(BST_node<RectKey, RectData> rt, RectData d) {
 		if (rt == null) return null;
 		
