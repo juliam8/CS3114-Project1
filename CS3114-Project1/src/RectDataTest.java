f7c1df511bae3c2eeea05db2518b2ac9b7896357
abbym1 <abbym1@vt.edu>
2019-02-07 12:36:50 -0500

    rectdata test created

---
+++ CS3114-Project1/src/RectDataTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RectDataTest {
+
+	@Test
+	public void testRectData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCoordinates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testW() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testH() {
+		fail("Not yet implemented");
+	}
+
+}
