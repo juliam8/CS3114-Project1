683822bf20bc92f2e323d42bfba330f5b69f8c7c
Julia Mahon Kuzin <juliam8@vt.edu>
2019-02-06 22:12:18 -0500

    changed func name

--- CS3114-Project1/src/BST_Rectangle.java
+++ CS3114-Project1/src/BST_Rectangle.java
@@ -46,19 +46,19 @@ public class BST_Rectangle<K, D> extends BST<RectKey, RectData> {
 	//height and width must be greater than 0
 	//can be outside 0 1024 range
 	void regionsearch(RectData d) {
-		regionsearch_helper(root, d);
+		regionsearch_traverse(root, d);
 	}
 	
 	//how about we just get the top left coordi
 	//l1 top left
 	//r1 bottom right
-	void regionsearch_helper(BST_node<RectKey, RectData> rt, RectData d) {
+	void regionsearch_traverse(BST_node<RectKey, RectData> rt, RectData d) {
 		if (rt == null) return;
 		
 		if (rt != null) {
-			regionsearch_helper(rt.left(), d);
+			regionsearch_traverse(rt.left(), d);
 			regionsearch_check(rt, d);
-			regionsearch_helper(rt.right(), d);
+			regionsearch_traverse(rt.right(), d);
 		}
 	}
 	
