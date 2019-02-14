45182d0a99d60b645403636271f2981a2cc3e857
abbym1 <abbym1@vt.edu>
2019-02-14 13:56:20 -0500

    formatting

--- CS3114-Project1/src/Parser.java
+++ CS3114-Project1/src/Parser.java
@@ -6,120 +6,129 @@ import java.util.Scanner;
  * @author juliam8
  * @author abbym1
  * @version 2019-02-14
- */
-
-/**
- * This object parses a file and interprets the contents
- * according to CS3114 Project 1 requirements
+ *
+ * This object parses a file and interprets the contents according to
+ * CS3114 Project 1 requirements
  */
 public class Parser {
-	/**
-	 * Parameterized constructor for the Parser class
-	 * Sets the private member variables my_bst and scan
-	 * @param f 	Path to the input file containing the commands
-	 * @param BST	Binary Search tree associated with 
-	 * 				the parser object and input file upon which the 
-	 * 				commands will be run
-	 */
-	Parser(File f, BST_Rectangle<RectKey, RectData> BST) {
-		try {
-			my_bst = BST;
-			scan = new Scanner(f);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * 
-	 */
-	public void execute() {
+    /**
+     * Parameterized constructor for the Parser class Sets the private member
+     * variables mBST and mScan
+     * 
+     * @param inputFile     Path to the input file containing the commands
+     * @param rectBST       Binary Search tree associated with the parser object and input
+     *                      file upon which the commands will be run
+     */
+    Parser(File inputFile, BST_Rectangle<RectKey, RectData> rectBST) {
+        try {
+            mBST = rectBST;
+            mScan = new Scanner(inputFile);
+        } 
+        catch (FileNotFoundException e) {
+            //Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-		while (scan.hasNext()) {
-			String command = scan.next();
+    /**
+     * Parses each command from the input file using a Scanner calls the appropriate
+     * helper function(s) to execute each command
+     */
+    public void execute() {
 
-			if (command.equals("insert")) {
-				insert();
-			} else if (command.equals("remove")) {
-				remove();
-			} else if (command.equals("regionsearch")) {
-				region_search();
-			} else if (command.equals("search")) {
-				my_bst.search(scan.next());
-			} else if (command.equals("intersections")) {
-				intersections();
-			} else if (command.equals("dump")) {
-				my_bst.dump();
-			}
-		}
-		scan.close();
-	}
+        while (mScan.hasNext()) {
+            String command = mScan.next();
 
-	private void insert() {
-		String name = scan.next();
-		int[] nums = new int[4];
-		for (int i = 0; i < 4; i++) {
-			nums[i] = Integer.parseInt(scan.next());
-		}
-		RectKey node_key = new RectKey(name);
-		RectData node_data = new RectData(nums);
-		my_bst.insert(node_key, node_data);
-		if ((nums[2] <= 0 || nums[3] <= 0) || (nums[0] + nums[2] > 1024 || nums[0] + nums[2] < 0
-				|| nums[1] + nums[3] > 1024 || nums[1] + nums[3] < 0)) {
-			System.out.print("Rectangle rejected: (" + node_key + " " + node_data + ")\n");
-		} else {
-			System.out.print("Rectangle accepted: (" + node_key + " " + node_data + ")\n");
-		}
-	}
+            if (command.equals("insert")) {
+                insert();
+            } 
+            else if (command.equals("remove")) {
+                remove();
+            } 
+            else if (command.equals("regionsearch")) {
+                region_search();
+            } 
+            else if (command.equals("search")) {
+                mBST.search(mScan.next());
+            } 
+            else if (command.equals("intersections")) {
+                intersections();
+            } 
+            else if (command.equals("dump")) {
+                mBST.dump();
+            }
+        }
+        mScan.close();
+    }
 
-	private void region_search() {
-		int[] nums = new int[4];
-		for (int i = 0; i < 4; i++) {
-			nums[i] = Integer.parseInt(scan.next());
-		}
-		if (nums[2] <= 0 || nums[3] <= 0) // command rejected if width/height is <= 0
-			System.out.print("Rectangles command rejected.");
-		else {
-			RectData node_data = new RectData(nums);
-			System.out.print("Rectangles intersecting region (" + node_data + "):\n");
-			my_bst.regionsearch(my_bst.root(), node_data);
-		}
-	}
+    /**
+     * Method to execute the BST insert command 
+     * Accepts or rejects the input rectangle based on 0,0 1024,1024 bounds
+     */
+    private void insert() {
+        String name = mScan.next();
+        int[] nums = new int[4];
+        for (int i = 0; i < 4; i++) {
+            nums[i] = Integer.parseInt(mScan.next());
+        }
+        RectKey nodeKey = new RectKey(name);
+        RectData nodeData = new RectData(nums);
+        mBST.insert(nodeKey, nodeData);
+        if ((nums[2] <= 0 || nums[3] <= 0) || (nums[0] + nums[2] > 1024 || nums[0] + nums[2] < 0
+                || nums[1] + nums[3] > 1024 || nums[1] + nums[3] < 0)) {
+            System.out.print("Rectangle rejected: (" + nodeKey + " " + nodeData + ")\n");
+        } else {
+            System.out.print("Rectangle accepted: (" + nodeKey + " " + nodeData + ")\n");
+        }
+    }
 
-	private void remove() {
-		if (scan.hasNextInt()) {
-			remove_data();
-		} else {
-			remove_key();
-		}
-	}
+    private void region_search() {
+        int[] nums = new int[4];
+        for (int i = 0; i < 4; i++) {
+            nums[i] = Integer.parseInt(mScan.next());
+        }
+        if (nums[2] <= 0 || nums[3] <= 0) // command rejected if width/height is <= 0
+            System.out.print("Rectangles command rejected.");
+        else {
+            RectData nodeData = new RectData(nums);
+            System.out.print("Rectangles intersecting region (" + nodeData + "):\n");
+            mBST.regionsearch(mBST.root(), nodeData);
+        }
+    }
 
-	private void remove_key() {
-		RectKey node_key = new RectKey(scan.next());
-		BST_node<RectKey, RectData> temp = my_bst.remove(node_key);
-		if (temp == null) {
-			System.out.print("Rectangle rejected (" + node_key + ")\n");
-		}
-	}
+    private void remove() {
+        if (mScan.hasNextInt()) {
+            remove_data();
+        } else {
+            remove_key();
+        }
+    }
 
-	private void remove_data() {
-		int[] nums = new int[4];
-		for (int i = 0; i < 4; i++) {
-			nums[i] = Integer.parseInt(scan.next());
-		}
-		RectData node_data = new RectData(nums);
-		BST_node<RectKey, RectData> temp = my_bst.remove(node_data); // send in array of integers
-		if (temp == null) {
-			System.out.print("Rectangle rejected (" + node_data + ")\n");
-		}
-	}
+    private void remove_key() {
+        RectKey nodeKey = new RectKey(mScan.next());
+        BST_node<RectKey, RectData> temp = mBST.remove(nodeKey);
+        if (temp == null) {
+            System.out.print("Rectangle rejected (" + nodeKey + ")\n");
+        }
+    }
 
-	private void intersections() {
-		System.out.print("Intersection pairs:\n");
-		my_bst.intersection();
-	}
+    private void remove_data() {
+        int[] nums = new int[4];
+        for (int i = 0; i < 4; i++) {
+            nums[i] = Integer.parseInt(mScan.next());
+        }
+        RectData nodeData = new RectData(nums);
+        BST_node<RectKey, RectData> temp = mBST.remove(nodeData); // send in array of integers
+        if (temp == null) {
+            System.out.print("Rectangle rejected (" + nodeData + ")\n");
+        }
+    }
 
-	private Scanner scan;
-	private BST_Rectangle<RectKey, RectData> my_bst;
+    private void intersections() {
+        System.out.print("Intersection pairs:\n");
+        mBST.intersection();
+    }
+
+    private Scanner mScan;
+    private BST_Rectangle<RectKey, RectData> mBST;
 }
