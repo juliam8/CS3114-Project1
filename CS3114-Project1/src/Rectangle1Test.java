b2985fe2a4f525356c32cdb24d5c2020e7f6e387
abbym1 <abbym1@vt.edu>
2019-02-14 14:53:06 -0500

    added testing

--- CS3114-Project1/src/Rectangle1Test.java
+++ CS3114-Project1/src/Rectangle1Test.java
@@ -12,12 +12,24 @@ public class Rectangle1Test extends TestCase {
 	 * Test method for {@link Rectangle1#main(java.lang.String[])}.
 	 */
 	public void testMain() {
-		String[] in = {"SyntaxTest.txt"};// "C:\\Users\\amalone46\\git\\CS3114-Project1\\p1\\CS3114-Project1\\src\\SyntaxTest.txt" };
+	    //String[] in = {"C:\\Users\\amalone46\\git\\CS3114-Project1\\p1\\CS3114-Project1\\src\\GeneralTest.txt" };
+		String[] in = {"GeneralTest.txt"};
 		try {
 			Rectangle1.main(in);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			// Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	public void testSyntax() {
+        //String[] in = {"C:\\Users\\amalone46\\git\\CS3114-Project1\\p1\\CS3114-Project1\\src\\SyntaxTest.txt" };
+        String[] in = {"SyntaxTest.txt"};
+        try {
+            Rectangle1.main(in);
+        } catch (FileNotFoundException e) {
+            // Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
