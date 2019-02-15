8c2354b23274ff66f5fa630013322788fcb3c22e
Julia Mahon Kuzin <juliam8@vt.edu>
2019-02-14 21:41:39 -0500

    finished BST testing

--- CS3114-Project1/src/BSTTest.java
+++ CS3114-Project1/src/BSTTest.java
@@ -20,7 +20,6 @@ public class BSTTest extends TestCase{
 	@Test
 	public void testBST() {
 		BST<RectKey, RectData> mytree = new BST<RectKey, RectData>();
-
 		assertEquals(mytree.nodeCount(), 0);
 		assertEquals(mytree.root(), null);
 	}
@@ -68,7 +67,6 @@ public class BSTTest extends TestCase{
 		assertEquals(mytree.root().right().key(), key2);
 		
 		assertEquals(mytree.nodeCount(), 3);
-
 	}
 
 	/**
@@ -76,7 +74,27 @@ public class BSTTest extends TestCase{
 	 */
 	@Test
 	public void testDump() {
-		// fail("Not yet implemented");
+        BST<RectKey, RectData> mytree = new BST<RectKey, RectData>();
+        RectKey key1 = new RectKey("e");
+        RectKey key2 = new RectKey("d");
+        RectKey key3 = new RectKey("f");
+        int[] c = { 1, 1, 4, 5 };
+        RectData d = new RectData(c);
+        mytree.insert(new BST_node<RectKey, RectData>(key1, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key2, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key3, d));
+        
+        mytree.dump();
+
+        assertEquals(mytree.root().data(), d);
+        assertEquals(mytree.root().left().data(), d);
+        assertEquals(mytree.root().right().data(), d);
+
+        assertEquals(mytree.root().key().name(), "e");
+        assertEquals(mytree.root().left().key().name(), "d");
+        assertEquals(mytree.root().right().key().name(), "f");
+        
+        assertEquals(mytree.nodeCount(), 3);
 	}
 
 	/**
@@ -84,7 +102,27 @@ public class BSTTest extends TestCase{
 	 */
 	@Test
 	public void testSearch() {
-		// fail("Not yet implemented");
+        BST<RectKey, RectData> mytree = new BST<RectKey, RectData>();
+        RectKey key1 = new RectKey("e");
+        RectKey key2 = new RectKey("d");
+        RectKey key3 = new RectKey("f");
+        int[] c = { 1, 1, 4, 5 };
+        RectData d = new RectData(c);
+        mytree.insert(new BST_node<RectKey, RectData>(key1, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key2, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key3, d));
+        
+        mytree.search("e");
+
+        assertEquals(mytree.root().data(), d);
+        assertEquals(mytree.root().left().data(), d);
+        assertEquals(mytree.root().right().data(), d);
+
+        assertEquals(mytree.root().key().name(), "e");
+        assertEquals(mytree.root().left().key().name(), "d");
+        assertEquals(mytree.root().right().key().name(), "f");
+        
+        assertEquals(mytree.nodeCount(), 3);
 	}
 
 	/**
@@ -92,7 +130,23 @@ public class BSTTest extends TestCase{
 	 */
 	@Test
 	public void testRoot() {
-		// fail("Not yet implemented");
+        BST<RectKey, RectData> mytree = new BST<RectKey, RectData>();
+        RectKey key1 = new RectKey("b");
+        RectKey key2 = new RectKey("a");
+        RectKey key3 = new RectKey("c");
+        int[] c = { 1, 1, 4, 5 };
+        RectData d = new RectData(c);
+        mytree.insert(new BST_node<RectKey, RectData>(key1, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key2, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key3, d));
+        
+        assertEquals(mytree.root().data(), d);
+        assertEquals(mytree.root().left().data(), d);
+        assertEquals(mytree.root().right().data(), d);
+
+        assertEquals(mytree.root().key().name(), "b");
+        assertEquals(mytree.root().left().key().name(), "a");
+        assertEquals(mytree.root().right().key().name(), "c");
 	}
 
 	/**
@@ -100,7 +154,23 @@ public class BSTTest extends TestCase{
 	 */
 	@Test
 	public void testnodeCount() {
-		// fail("Not yet implemented");
+        BST<RectKey, RectData> mytree = new BST<RectKey, RectData>();
+        RectKey key1 = new RectKey("e");
+        RectKey key2 = new RectKey("d");
+        RectKey key3 = new RectKey("f");
+        RectKey key4 = new RectKey("g");
+        RectKey key5 = new RectKey("h");
+        RectKey key6 = new RectKey("i");
+        int[] c = { 1, 1, 4, 5 };
+        RectData d = new RectData(c);
+        mytree.insert(new BST_node<RectKey, RectData>(key1, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key2, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key3, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key4, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key5, d));
+        mytree.insert(new BST_node<RectKey, RectData>(key6, d));
+        
+        assertEquals(mytree.nodeCount(), 6);
 	}
 
 }
